431f3ea0c81cb8c664edb7c15db9234e7e075fb6
svvac <_@svvac.net>
2017-01-05 01:11:54 +0100

    Add split-buffer

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Then
 
     const aze = require('aze');
 
-## API
+## Core API
 
 ### ``aze.get(obj, key, def)``
 Lookup `key` in `obj` and return `def` if not found.
@@ -28,4 +28,36 @@ Lookup `key` in `obj` and return `def` if not found.
 keys. The separator can be changed at `aze.get.SEPARATOR`.
 
     let timeout = aze.get(config, 'timeout', 1000);
+
+
+## Additional utils
+
+### `SplitBuffer(guard, cb, repeat)`
+
+Feed chunks of data to a `SplitBuffer` and it will emit slices when it
+encounters `guard`.
+
+Unless `repeat` is true, `cb` will only be called upon encountering `guard` the
+first time.
+
+`cb` is called with a buffer containing all data up to the guard (inclusive),
+and a buffer with the remaining data.
+
+Call the `.feed()` method to push data.
+
+    const SplitBuffer = require('aze/split-buffer');
+
+    function cb(slice, left) {
+        console.log(`${slice.length} bytes before yolo. ${left.size} remaining`));
+    }
+
+    let buf = new SplitBuffer("yolo", cb);
+
+    buf.feed("blah blah blah blah blah blah blah blah blah blah ");
+    buf.feed("blah blah blah blah blah blah blah blah blah blah ");
+    buf.feed("blah blah blah blah yolo blah blah blah blah blah ");
+    // 124 bytes before yolo. 26 remaining
+    buf.feed("blah blah blah blah yolo blah blah blah blah blah ");
+    // If `repeat` were set on buf, it would yield:
+    // 50 bytes before yolo. 26 remaining
 
---
+++ split-buffer.js
@@ -0,0 +1,67 @@
+/* Copyright 2016 Simon `svvac` Wachter (_@svvac.net)
+
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENaze.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+class SplitBuffer {
+    constructor(guard, callback, repeat) {
+        this.guard = guard;
+        this.buf = Buffer.alloc(0);
+        this.cb = callback;
+        this.offset = 0;
+        this.repeat = !!repeat;
+        this.done = false;
+    }
+
+    feed(data) {
+        if (this.done) return;
+
+        if (data) {
+            data = Buffer.from(data);
+            this.buf = Buffer.concat([ this.buf, data ], this.buf.length + data.length);
+        } else {
+            data = '';
+        }
+
+        let idx = this.buf.indexOf(this.guard, this.offset);
+        if (idx > -1) {
+            idx += this.guard.length;
+            const slice = this.buf.slice(0, idx);
+            const leftover = this.buf.slice(idx);
+
+            this.buf = leftover;
+            this.offset = 0;
+
+            setImmediate(() => this.cb(slice, leftover));
+
+            if (this.repeat) {
+                setImmediate(() => this.feed());
+            } else {
+                this.done = true;
+            }
+
+        } else {
+            this.offset = this.buf.length;
+        }
+    }
+}
+
+module.exports = SplitBuffer;
+
